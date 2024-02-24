8af81074c10bd405d4a7263adec33dce101ab984
kevinkennedy <@kennedy.kev@northeastern.edu>
2024-02-23 23:57:45 -0500

    abc

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/"         element={<Navigate to="/Labs"/>}/>
-          <Route path="/Labs/*"   element={<Labs/>}/>
+          <Route path="/"         element={<Navigate to="/kanbas"/>}/>
           <Route path="/kanbas/*" element={<Kanbas/>}/>
           <Route path="/hello"    element={<HelloWorld/>}/>
         </Routes>
