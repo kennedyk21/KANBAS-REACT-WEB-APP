895b25e212bd5f214b784cb95caeddb3128f0e52
kevinkennedy <@kennedy.kev@northeastern.edu>
2024-04-18 02:56:04 -0400

    1

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,4 +1,5 @@
 import KanbasNavigation from "./Navigation";
+import Account from "./Account";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Courses from "./Courses";
@@ -59,7 +60,7 @@ function Kanbas() {
       <div style={{ flexGrow: 1 }}>
         <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Account/*" element={<Account />} />
             <Route path="Dashboard" element={            
             <Dashboard
               courses={courses}
